57a8c522945d165f6a24299772f0c0cbe01684b4
MohamedHany17m8 <mhany6921@gmail.com>
2025-03-25 15:35:43 +0200

    proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://x-api-delta.vercel.app",
         changeOrigin: true,
       },
     },
